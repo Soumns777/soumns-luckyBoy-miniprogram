26172776b5b03bc5ad64fac3697ae73cd8374fa5
root <root@soumns.localdomain>
2022-01-24 20:56:11 +0800

    luckyBoy-init

---
+++ pages/cart/cart.vue
@@ -0,0 +1,11 @@
+<template>
+  <view class="container">
+    分类
+  </view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/home/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <view class="container">
+     首页
+  </view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
